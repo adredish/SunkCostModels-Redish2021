918cdcc9fe5c38b898529a0d40e867ba48f1dd44
David Redish <redish@umn.edu>
2022-02-17 09:01:02 -0600

    Fixed Attrition bias bug

--- KPShowAttritionBias.m
+++ KPShowAttritionBias.m
@@ -4,20 +4,20 @@ assert(all(isnan(R.isEarn)==isnan(R.isQuit)));
 k = isnan(R.isEarn);
 R.isEarn(k) = false; R.isQuit(k) = false; R.TSQ(k) = -5;
 
-H = nan(30,30);
+H = nan(30,40);
 mu = nan(30,1);
 se = nan(30,1);
 
 for iTS = 1:30
-    stillAt = k & (R.isEarn & (R.offer > iTS)) | (R.isQuit & (R.TSQ > iTS));
-    H(iTS,:) = histcounts(R.W0(stillAt),0.5:1:30.5);
-    H(iTS,:) = H(iTS,:) ./ sum(H(iTS,:));
+    stillAt = ~k & (R.isEarn & (R.offer > iTS)) | (R.isQuit & (R.TSQ > iTS));
+    H(iTS,:) = histcounts(R.W0(stillAt),0.5:1:40.5);
+    H(iTS,:) = H(iTS,:) ./ sum(H(iTS,:), 'omitnan');
     mu(iTS) = mean(R.W0(stillAt));
     se(iTS) = nanstderr(R.W0(stillAt));
 end
 
 clf;
-imagescWnan(H');
+imagesc(H');
 axis xy
 hold on;
 errorbar(1:30, mu, se, 'k');
